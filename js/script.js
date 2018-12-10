113a0ad6f64164ec170117bf07a56b9bc2ddc78f
Polska Panda <cookavsky@gmail.com>
2018-12-10 23:48:53 +0100

    Create Intro

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
-const RNum = 38 - (Math.floor(Math.random() * 19));
+const button = document.getElementsByClassName('Intro');
 
 
+window.onload = function() {
+    for (let i = 0; i < button.length; i++) {
+        button[i].addEventListener('click', function () {
+            button[i].classList.add("Back");
+            button[i].childNodes[1].classList.remove("Hide")
+            button[i].classList.add("Back");
+            button[i].childNodes[3].classList.remove("Hide")
+        })
+}}
