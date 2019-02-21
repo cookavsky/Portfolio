b7aec47e7e092d9fd5a60d8e16db952644f66960
cookavsky <cookavsky@gmail.com>
2019-02-21 21:24:28 +0100

    - continue

--- js/start-end.js
+++ js/start-end.js
@@ -1,177 +1,159 @@
-const Table = ["one", "two", "three"];
 let Cards = document.querySelectorAll(".Cards");
+let CardsStart = document.querySelectorAll(".CardsStart");
+let CardsEnd = document.querySelectorAll(".CardsEnd");
 
 console.log(Cards);
 
-const Game = function() {
-    greenOne = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-    // first RIGHT green throw
-    if (greenOne === 1) {
-        Cards[0].classList.add("one");
-        blueOne = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-        // first RIGHT blue throw
-        if (blueOne === 1) {
-            Cards[1].classList.add("two");
-            redOne = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-            // frist RIGHT red throw
-            if (redOne === 1) {
-                Cards[2].classList.add("three");
-                greenTwo = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                // two RIGHT green throw
-                if (greenTwo === 1) {
-                    Cards[3].classList.add("one");
-                    // two BOTTOM blue throw
-                    Cards[8].classList.add("two");
-                    redTwo = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                    // two BOTTOM red throw
-                    if (redTwo === 1) {
-                        Cards[13].classList.add("three");
-                        greenThree = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                        // three BOTTOM green throw
-                        if (greenThree === 1) {
-                            Cards[18].classList.add("one");
-                            blueThree = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                            // three LEFT blue throw
-                            if (blueThree === 1) {
-                                Cards[17].classList.add("two");
-                                redThree = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                // three TOP red throw
-                                if (redThree === 1) {
-                                    Cards[12].classList.add("three");
-                                    greenFour = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                    // Four TOP green throw
-                                    if (greenFour === 1) {
-                                        Cards[7].classList.add("one");
-                                        // Four LEFT blue throw
-                                        Cards[6].classList.add("two");
-                                        redFour = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                        // Four LEFT red throw
-                                        if (redFour === 1) {
-                                            Cards[5].classList.add("three");
-                                            greenFive = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                            // Five LEFT green throw
-                                            if (greenFive === 1) {
-                                                Cards[4].classList.add("one");
-                                                // Five BOTTOM blue throw
-                                                Cards[9].classList.add("two");
-                                                redFive = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                                // Five RIGHT red throw
-                                                if (redFive === 1) {
-                                                    Cards[10].classList.add("three");
-                                                    greenSix = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                                    // Six RIGHT green throw
-                                                    if (greenSix === 1) {
-                                                        Cards[11].classList.add("one");
-                                                        // Six BOTTOM blue throw
-                                                        Cards[16].classList.add("two");
-                                                        redSix = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
-                                                        // Six BOTTOM red throw
-                                                        if (redSix === 1) {
-                                                            Cards[21].classList.add("three");
-                                                            // Seven LEFT green throw
-                                                            Cards[20].classList.add("one");
-                                                            // Seven LEFT blue throw
-                                                            Cards[19].classList.add("two");
-                                                            // Seven BOTTOM blue throw
-                                                            Cards[24].classList.add("three");
-                                                            // Seven RIGHT green throw
-                                                            Cards[25].classList.add("one");
-                                                            // Seven RIGHT blue throw
-                                                            Cards[26].classList.add("two");
-                                                            // Seven RIGHT red throw
-                                                            Cards[27].classList.add("three");
-                                                        // Six LEFT red throw
-                                                        } else if (redSix === 2) {
-
-                                                        }
-                                                    // Six BOTTOM green throw
-                                                    } else if (greenSix === 2) {
-
-                                                    }
-                                                // Five BOTTOM red throw
-                                                } else if (redFive === 2) {
-
-                                                }
-                                            // Five BOTTOM green throw
-                                            } else if (greenFive === 2) {
-
-                                            }
-                                        // Four BOTTOM red throw
-                                        } else if (redFour === 2) {
-
-                                        }
-                                    // Four LEFT green throw
-                                    } else if (greenFour === 2) {
-                                        Cards[11].classList.add("one");
-                                    }
-                                // three LEFT red throw
-                                } else if (redThree === 2) {
-
-                                }
-                            // three BOTTOM blue throw
-                            } else if (blueThree === 2) {
-
-                            }
-                        // three LEFT green throw
-                        } else if (greenThree === 2) {
-                            Cards[12].classList.add("one");
-                        }
-                    // two LEFT red throw
-                    } else if (redTwo === 2) {
-
-                    }
-                // two BOTTOM green throw
-                } else if (greenTwo === 2) {
-                    Cards[7].classList.add("one");
-                    // two blue throw
-                    blueTwo = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-                    if (blueTwo === 1) {
-                        Cards[8].classList.add("two");
-                    // two red throw
-                        redTwo = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-                    } else if (blueTwo === 2) {
-                        Cards[12].classList.add("two");
-                    // two red throw
-                        redTwo = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-                    } else if (blueTwo === 3) {
-                        Cards[6].classList.add("two");
-                    // two red throw
-                        redTwo = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
-                    }
+const Game = function () {
+    for (let y = 0; y < CardsStart.length; y++) {
+        CardsStart[y].addEventListener("click", function () {
+            Cards[0].classList.add("green");
+            Cards[1].classList.add("red");
+            Cards[2].classList.add("blue");
+            Cards[3].classList.add("red");
+            Cards[4].classList.add("blue");
+            Cards[5].classList.add("blue");
+            Cards[6].classList.add("red");
+            Cards[7].classList.add("green");
+            Cards[8].classList.add("green");
+            Cards[9].classList.add("green");
+            Cards[10].classList.add("red");
+            Cards[11].classList.add("blue");
+            Cards[12].classList.add("red");
+            Cards[13].classList.add("blue");
+            Cards[14].classList.add("blue");
+            Cards[15].classList.add("blue");
+            Cards[16].classList.add("blue");
+            Cards[17].classList.add("green");
+            Cards[18].classList.add("green");
+            Cards[19].classList.add("red");
+            Cards[20].classList.add("green");
+            Cards[21].classList.add("red");
+            Cards[22].classList.add("blue");
+            Cards[23].classList.add("red");
+            Cards[24].classList.add("green");
+            Cards[25].classList.add("blue");
+            Cards[26].classList.add("red");
+            Cards[27].classList.add("green");
+        });
+    for (let i = 0; i < Cards.length; i++) {
+            Cards[i].addEventListener("click", function () {
+                console.log(i, Cards[i].className)
+                // Road 1
+                if (i === 0 && Cards[i].className === "Cards green") {
+                    Cards[0].classList.remove("green");
+                    Cards[0].classList.add("grey");
+                // Road 1
+                } else if (i === 5 && Cards[0].className === "Cards grey") {
+                    Cards[5].classList.remove("blue");
+                    Cards[5].classList.add("grey");
+                // Road 1
+                } else if (i === 6 && Cards[0].className === "Cards grey" && Cards[5].className === "Cards grey" && Cards[10].className === "Cards red") {
+                    Cards[6].classList.remove("red");
+                    Cards[6].classList.add("grey");
+                // Road 1
+                } else if (i === 7 && Cards[6].className === "Cards grey") {
+                    Cards[7].classList.remove("green");
+                    Cards[7].classList.add("grey");
+                // Road 1
+                } else if (i === 2 && Cards[7].className === "Cards grey") {
+                    Cards[2].classList.remove("blue");
+                    Cards[2].classList.add("grey");
+                // Road 1
+                } else if (i === 3 && Cards[2].className === "Cards grey") {
+                    Cards[3].classList.remove("red");
+                    Cards[3].classList.add("grey");
+                // Road 1
+                } else if (i === 8 && Cards[3].className === "Cards grey") {
+                    Cards[8].classList.remove("green");
+                    Cards[8].classList.add("grey");
+                // Road 1
+                } else if (i === 13 && Cards[8].className === "Cards grey") {
+                    Cards[13].classList.remove("blue");
+                    Cards[13].classList.add("grey");
+                // Road 1
+                } else if (i === 12 && Cards[13].className === "Cards grey") {
+                    Cards[12].classList.remove("red");
+                    Cards[12].classList.add("grey");
+                // Road 1
+                } else if (i === 17 && Cards[12].className === "Cards grey") {
+                    Cards[17].classList.remove("green");
+                    Cards[17].classList.add("grey");
+                // Road 1
+                } else if (i === 16 && Cards[17].className === "Cards grey" && Cards[22].className === "Cards blue") {
+                    Cards[16].classList.remove("blue");
+                    Cards[16].classList.add("grey");
+                // Road 1
+                } else if (i === 21 && Cards[16].className === "Cards grey") {
+                    Cards[21].classList.remove("red");
+                    Cards[21].classList.add("grey");
+                // Road 1
+                } else if (i === 20 && Cards[16].className === "Cards grey") {
+                    Cards[20].classList.remove("green");
+                    Cards[20].classList.add("grey");
+                // Road 1
+                } else if (i === 15 && Cards[16].className === "Cards grey" && Cards[25].className === "Cards blue") {
+                    Cards[15].classList.remove("blue");
+                    Cards[15].classList.add("grey");
+                // Road 1
+                } else if (i === 10 && Cards[15].className === "Cards grey") {
+                    Cards[10].classList.remove("red");
+                    Cards[10].classList.add("grey");
+                // Road 1
+                } else if (i === 9 && Cards[10].className === "Cards grey") {
+                    Cards[9].classList.remove("green");
+                    Cards[9].classList.add("grey");
+                // Road 1
+                } else if (i === 14 && Cards[9].className === "Cards grey") {
+                    Cards[14].classList.remove("blue");
+                    Cards[14].classList.add("grey");
+                // Road 1
+                } else if (i === 19 && Cards[14].className === "Cards grey") {
+                    Cards[19].classList.remove("red");
+                    Cards[19].classList.add("grey");
+                // Road 1
+                } else if (i === 24 && Cards[19].className === "Cards grey") {
+                    Cards[24].classList.remove("green");
+                    Cards[24].classList.add("grey");
+                // Road 1
+                } else if (i === 25 && Cards[24].className === "Cards grey") {
+                    Cards[25].classList.remove("blue");
+                    Cards[25].classList.add("grey");
+                // Road 1
+                } else if (i === 26 && Cards[25].className === "Cards grey") {
+                    Cards[26].classList.remove("red");
+                    Cards[26].classList.add("grey");
+                // Road 1
+                } else if (i === 27 && Cards[26].className === "Cards grey") {
+                    Cards[27].classList.remove("green");
+                    Cards[27].classList.add("grey");
+                // Road 1
+                } else if (i === 22 && Cards[27].className === "Cards grey") {
+                    Cards[22].classList.remove("blue");
+                    Cards[22].classList.add("grey");
+                // Road 1
+                } else if (i === 23 && Cards[22].className === "Cards grey") {
+                    Cards[23].classList.remove("red");
+                    Cards[23].classList.add("grey");
                 }
-            // first BOTTOM red throw
-            } else if (redOne === 2) {
-                Cards[6].classList.add("three");
-            }
-        // first BOTTOM blue throw
-        } else if (blueOne === 2) {
-            Cards[5].classList.add("two");
-
-        }
-    // first BOTTOM green throw
-    } else if (greenOne === 2) {
-        Cards[4].classList.add("one");
+            })
+        };
     }
 }
 
-const AAA = function () {
-    for(let i = 0; i < Cards.length; i++) {
-        Cards[i].addEventListener("click", function() {
-            console.log(i)
-            if (Cards[i].className === "Cards") {
-                Cards[i].classList.add("one");
-            } else if (Cards[i].className === "Cards one") {
-                Cards[i].classList.remove("one");
-                Cards[i].classList.add("two");
-            } else if (Cards[i].className === "Cards two") {
-                Cards[i].classList.remove("two");
-                Cards[i].classList.add("three");
-            } else if (Cards[i].className === "Cards three") {
-                Cards[i].classList.remove("three");
-                Cards[i].classList.add("one");
-            }
-        })
-    }
-}
+Game()
 
-AAA();
+                // Road 1
+// Right -> Bottom -> Right -> Right -> Top -> Right -> Bottom -> Bottom -> Left -> Bottom -> Left -> Bottom -> Left -> Top -> Top -> Left -> Bottom -> Bottom -> Bottom -> Right -> Right -> Right -> Top -> Right -> |END|
+
+                // Road 2
+// Right -> Bottom -> Right -> Right -> Top -> Right -> Bottom -> Bottom -> Left -> Bottom -> Left -> Bottom -> Left
+
+                // Road 3
+// Right -> Bottom -> Right -> Right -> Top -> Right -> Bottom -> Bottom -> Left -> Bottom ->
+
+                // Road 4
+// Right -> Bottom -> Bottom -> Left -> Bottom -> Bottom -> Bottom -> Right -> Right -> Right -> Top -> Right -> |END|
+
+                // Road 5
+// Right -> Bottom -> Bottom -> Left -> Bottom -> Bottom -> Right -> Bottom -> Right -> Right -> Top -> Right -> |END|
